e209084103ec7af3ab643e8c12b66ee690531f52
Sparrow0hawk <alexjcoleman@gmail.com>
2023-07-06 09:03:26 +0100

    add login required wrapper

--- simple_flask/auth.py
+++ simple_flask/auth.py
@@ -91,3 +91,14 @@ def load_logged_in_user() -> None:
 def logout() -> Response:
     session.clear()
     return redirect(url_for("index"))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        
+        return view(**kwargs)
+
+    return wrapped_view
