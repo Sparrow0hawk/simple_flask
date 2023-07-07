5471475b0cbd72c9d4c2e2e9912008285ab44d93
Sparrow0hawk <alexjcoleman@gmail.com>
2023-07-07 08:10:38 +0100

    add edit and delete form

--- simple_flask/forms.py
+++ simple_flask/forms.py
@@ -7,3 +7,12 @@ class CreatePostForm(FlaskForm):
     title = StringField("title", validators=[DataRequired()])
     body = TextAreaField("body", validators=[DataRequired()])
     submit = SubmitField()
+
+
+class EditPostForm(CreatePostForm):
+    submit = SubmitField("Save")
+ 
+    
+class DeletePostForm(FlaskForm):
+    delete = SubmitField("Delete",
+                        render_kw={"onclick": "return confirm('Are you sure?')"})
