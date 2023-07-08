740bfe4b395a5dba5b4f5f4ae924ae7390d7d59c
Sparrow0hawk <alexjcoleman@gmail.com>
2023-07-08 14:38:43 +0100

    add tests for login and logout

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -30,3 +30,34 @@ def test_register_validate_input(client, username, password, message):
         "/auth/register", data={"username": username, "password": password}
     )
     assert message in res.data
+
+
+def test_login(client, auth):
+    assert client.get("/auth/login").status_code == 200
+    res = auth.login()
+    assert res.headers["Location"] == "/"
+
+    with client:
+        client.get("/")
+        assert session["user_id"] == 1
+        assert g.user["username"] == "test"
+
+
+@pytest.mark.parametrize(
+    ("username", "password", "message"),
+    (
+        ("a", "test", b"Incorrect username"),
+        ("test", "a", b"Incorrect password"),
+    ),
+)
+def test_login_validate_input(auth, client, username, password, message):
+    res = auth.login(username, password)
+    assert message in res.data
+
+
+def test_logout(client, auth):
+    auth.login()
+
+    with client:
+        auth.logout()
+        assert "user_id" not in session
